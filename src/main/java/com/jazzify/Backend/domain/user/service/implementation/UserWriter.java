72df781589857a587032973c1abdcefd24ff6d1a
KEEKE132 <dksvlfdhs1@gmail.com>
2026-03-09 00:13:00 +0900

    chore: 패키지 리팩토링

--- src/main/java/com/jazzify/Backend/domain/user/service/implementation/UserWriter.java
+++ src/main/java/com/jazzify/Backend/domain/user/service/implementation/UserWriter.java
@@ -2,10 +2,9 @@ package com.jazzify.backend.domain.user.service.implementation;
 
 import com.jazzify.backend.domain.user.entity.User;
 import com.jazzify.backend.domain.user.repository.UserRepository;
-import com.jazzify.backend.domain.user.util.UserMapper;
 import lombok.RequiredArgsConstructor;
-
 import org.jspecify.annotations.NullMarked;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserWriter {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public User create(String username, String rawPassword) {
-        User user = userMapper.toEntity(username, rawPassword);
+        User user = User.builder()
+                .username(username)
+                .password(passwordEncoder.encode(rawPassword))
+                .build();
         return userRepository.save(user);
     }
 }
