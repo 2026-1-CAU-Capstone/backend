13a770fb954e51e92182cf10dab333d731e42a4b
KEEKE132 <dksvlfdhs1@naver.com>
2026-03-25 16:51:30 +0900

    feat: 테스트 설정

--- src/test/java/com/jazzify/backend/BackendApplicationTests.java
+++ src/test/java/com/jazzify/backend/BackendApplicationTests.java
@@ -2,9 +2,14 @@ package com.jazzify.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@MockitoBean
+	StringRedisTemplate stringRedisTemplate;
 
 	@Test
 	void contextLoads() {
