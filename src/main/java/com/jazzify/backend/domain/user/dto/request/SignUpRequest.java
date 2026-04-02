ba8bed792d413b28a62dac9118f0d452fb628984
KEEKE132 <dksvlfdhs1@naver.com>
2026-04-02 14:41:30 +0900

    fix: 비밀번호 형식 수정

--- src/main/java/com/jazzify/backend/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/com/jazzify/backend/domain/user/dto/request/SignUpRequest.java
@@ -18,7 +18,7 @@ public record SignUpRequest(
 
 	@NotBlank(message = "비밀번호는 필수입니다.")
 	@Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다.")
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$",
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
 		message = "비밀번호는 영문자와 숫자를 모두 포함해야 합니다.")
 	String password
 ) {
